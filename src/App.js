b09bbe40d2251074c7e2cc593b3a35d34ce45f50
Héctor BlisS <contacto@fixter.org>
2019-02-28 11:56:27 -0600

    🥑

--- src/App.js
+++ src/App.js
@@ -2,13 +2,14 @@ import React, { Component } from 'react';
 import './App.css';
 import Routes from './Routes';
 import { NavLink } from 'react-router-dom'
+import axios from 'axios'
 
 class App extends Component {
   render() {
     return (
       <div>
         <nav>
-          <NavLink to="/home" >
+          <NavLink to="/" >
             Inicio
           </NavLink>
           {" | "}
@@ -19,6 +20,14 @@ class App extends Component {
           <NavLink to="/login" >
             Inicia sesión
           </NavLink>
+          <button onClick={() => {
+            axios.get("http://localhost:3000/logout", { withCredentials: true })
+              .then(res => {
+                console.log(res)
+              })
+          }} >
+            Cerrar sesión
+          </button>
         </nav>
         <Routes />
       </div>
--- src/components/Home.js
+++ src/components/Home.js
@@ -1,3 +1,38 @@
 import React from 'react'
+import axios from 'axios'
 
-export default () => <div>Home</div>
+class Home extends React.Component {
+    state = {
+        privateInfo: {},
+        authorized: false
+    }
+
+    componentDidMount() {
+        this.getPrivateData()
+    }
+
+    getPrivateData = () => {
+        let url = "http://localhost:3000/private"
+        axios.get(url, { withCredentials: true })
+            .then(res => {
+                console.log(res)
+                this.setState({ privateInfo: res.data })
+            })
+            .catch(e => {
+                console.log(e)
+                this.props.history.push('/login')
+            })
+    }
+
+    render() {
+        let { privateInfo } = this.state
+        return (
+            <div>
+                <h1>Home</h1>
+                <h2>{privateInfo.message}</h2>
+            </div>
+        )
+    }
+}
+
+export default Home
